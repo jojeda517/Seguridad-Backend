d55742288f33876ca28787e1357283cd65fd7aeb
Andrés Tapia González <andrestgonza@gmail.com>
2023-12-28 10:15:26 -0500

    Solucionado cors

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from routes.facultad_route import facultad_router
 from routes.estudiante_route import estudiante_router
 from routes.carrera_route import carrera_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(facultad_router, tags=["Facultad"], prefix="/api/v1")
 app.include_router(estudiante_router, tags=["Estudiante"], prefix="/api/v1")
