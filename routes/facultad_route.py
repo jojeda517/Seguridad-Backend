b274e479dd56d94ae998b468f876ed0d17f2d931
José Ojeda <jojeda5171@uta.edu.ec>
2024-01-10 23:25:10 -0500

    Post y get de logo

--- routes/facultad_route.py
+++ routes/facultad_route.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException, Path, UploadFile, File, Depends
+from fastapi.responses import FileResponse
 from fastapi import Depends
 from config.connection import get_db
 from sqlalchemy.orm import Session
 from schemas.schemas import FacultadSchema, ResponseSchema
+from models.models import Facultad
+import os
 
 from repositories.facultad_repository import FacultadRepository
 
@@ -59,3 +62,49 @@ def delete_facultad(facultad_id: int = Path(..., gt=0), db: Session = Depends(ge
         message="Facultad eliminada exitosamente",
         result=db_facultad
     )
+
+
+@facultad_router.post("/logo/{facultad_id}")
+def upload_file(file: UploadFile = File(...), facultad_id: int = 0, db: Session = Depends(get_db)):
+    # Directorio principal
+    directorio_base = "Documents"
+    os.makedirs(directorio_base, exist_ok=True)
+    extension = os.path.splitext(file.filename)[1]
+
+    datos_facultad = db.query(Facultad).filter(
+        Facultad.id == facultad_id).first()
+
+    # Construir la estructura de carpetas
+    folder_structure = f"{datos_facultad.nombre}"
+
+    # Directorio completo
+    directorio_completo = os.path.join(directorio_base, folder_structure)
+
+    # Crea la estructura de carpetas si no existen
+    os.makedirs(directorio_completo, exist_ok=True)
+
+    # Ruta donde se almacenará el documento
+    file_path = os.path.join(
+        directorio_completo, datos_facultad.nombre+extension)
+
+    # Guardar el archivo en la ruta especificada
+    with open(file_path, "wb") as image:
+        image.write(file.file.read())
+
+    # Actualizar la ruta del archivo en la base de datos
+    db_documento = FacultadRepository.update_file(
+        db=db, facultad_id=facultad_id, file=file_path)
+
+    print(db_documento)
+    return db_documento
+
+
+@facultad_router.get("/logo/{facultad_id}")
+async def get_logo(facultad_id: str, db: Session = Depends(get_db)):
+    db_documento = FacultadRepository.get_facultad(db, facultad_id)
+    directorio = db_documento.logo
+    file_path = os.path.join(directorio)
+    if os.path.exists(file_path):
+        return FileResponse(file_path)
+    else:
+        return file_path
